2cde141b6a4ade8525cf66237677a6a4673e4f80
TiagoBryan <tiagobryanroliveira@gmail.com>
2023-03-30 14:12:58 -0300

    atualizaçao

--- Questionario3/src/main/java/com/mycompany/questionario3/Questionario3.java
+++ Questionario3/src/main/java/com/mycompany/questionario3/Questionario3.java
@@ -14,20 +14,39 @@ import javax.swing.JOptionPane;
 public class Questionario3 {
 
     public static void main(String[] args) {
+        int sub = 0;
+        int soma =0;
         int sexo = Integer.parseInt(JOptionPane.showInputDialog("Digite '1' se você for do sexo masculino ou '2' para feminino"));
+        int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua idade: "));
         if((sexo > 2) || (sexo<1)){
             JOptionPane.showMessageDialog(null, "Opção invalida");
             System.exit(0);
         }else{
         float alt = Float.parseFloat(JOptionPane.showInputDialog("Digite a sua altura em metros"));
-        float pi = calcularPesoIdeal(sexo, alt);
-        JOptionPane.showMessageDialog(null, "Seu peso ideal deve ser "+ pi+"KG");}
+        float pi = calcularPesoIdeal(sexo, alt, idade);
+        if((idade >=20) && (idade<=59)){
+            soma = 7;
+            sub = 11;
+        }else{
+            if(idade< 20){
+                soma = 3;
+             sub = 16;
+            }else{
+                soma = 12;
+             sub = 1;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Seu peso ideal deve estar entre "+ (pi+soma) +"KG e "+(pi-sub));}
     }
-    static float calcularPesoIdeal(int s, float altura){
+    static float calcularPesoIdeal(int s, float altura, int id){
         float pesoideal;
+        float pesoideal2;
         if(s == 1){
-             pesoideal = (float) (72.7 * altura)-58;
+            
+             pesoideal = ((float) (72.7 * altura)-58);
+             pesoideal2 = ((float) (72.7 * altura)-58);
             return pesoideal;
+            
         }else{
             if(s == 2){
                 pesoideal = (float) ((float) (62.1 * altura)-44.7);
