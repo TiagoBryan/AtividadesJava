d0cfeba2c473f92e548b2011dc2f6220946da856
TiagoBryan <tiagobryanroliveira@gmail.com>
2023-04-20 14:50:22 -0300

    Atualizando

---
+++ AULAexemplo20_04/src/main/java/com/mycompany/aulaexemplo20_04/exemplo2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aulaexemplo20_04;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ *
+ * @author dti
+ */
+public class exemplo2 extends JFrame{
+    JPasswordField caixa;
+    JLabel rotulo;
+    public exemplo2(){
+        super("Exemplo com JPasswordField");
+        Container tela = getContentPane();
+        setLayout(null);
+        rotulo = new JLabel("Senha: ");
+        caixa = new JPasswordField("10");
+        rotulo.setBounds(50, 20, 100, 20);
+        caixa.setBounds(50, 60, 100, 20);
+        tela.add(rotulo);
+        tela.add(caixa);
+        setSize(400,250);
+        setVisible(true);
+           
+    }
+    public static void main(String args[]){
+        exemplo2 app = new exemplo2();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    }
+}
